4e6eca1a421f43d483e7f3d304c09bfa13774f7e
said.morales.contractor <said.morales.contractor@bbva.com>
2018-03-25 12:14:48 -0600

    resolvers added

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const express =require('express')
+const bodyparser = require('body-parser')
+
+const { graphqlExpress, graphiqlExpress} = require('graphql-server-express')
+const schema = require('./schema')
+
+const app = express()
+
+app.use('/graphql',
+  bodyparser.json(),
+  graphqlExpress({schema})
+)
+
+app.use('/graphiql',
+  graphiqlExpress({
+    endpointURL:'graphql'
+  })
+)
+
+const PORT = 7070
+
+app.listen(PORT,() => {
+  console.log("Server startted OK")    
+})
+
---
+++ schema.js
@@ -0,0 +1,94 @@
+const { makeExecutableSchema } = require('graphql-tools')
+
+const typeDefs =`
+# Cusrsos actuales
+type Curso {
+    id: ID!
+    # Titulo o descripcion corta del curso
+    titulo: String
+    # Descripcion larga del curso
+    descripcion: String
+    # Profesor que ha impartido un curso
+    profesor: Profesor
+    # Que tanto ha gustado un curso
+    rating: Float @deprecated(reason: "Ya no se califican por clics si no por numero de visitas")
+    # Comentarios hechos a un curso
+    comentarios: [Comentario]
+}
+#Profesores y sus cursos
+type Profesor{
+    id: ID!
+    nombre: String
+    nacionalidad: String
+    genero: Genero
+    cursos: [Curso]
+}
+
+type Comentario{
+    id: ID!
+    nombre: String
+    cuerpo: String
+}
+
+
+enum Genero{
+    MASCULINO
+    FEMENINO
+}
+
+
+type Query{
+  cursos: [Curso]
+  profesores: [Profesor]
+  curso(id: Int): Curso
+  profesor(id: Int): Profesor
+  
+}
+`
+
+const resolvers = {
+  Query:{
+    cursos: () =>{
+      return [
+        {
+          id: 1,
+          titulo: "Spring framework",
+          descripcion: "Learn full description about spring",
+        },
+        {
+          id: 2,
+          titulo: "Cordoba",
+          descripcion: "Cordoba server",
+        }
+      ]
+    }
+  },
+  Curso:{
+    profesor: () =>{
+      return {
+        nombre: "Pablo"
+      }
+    },
+    comentarios: () =>{
+      return [
+        {
+          
+            id: "1",
+            nombre: "Alfa"
+        }
+      ]
+    }
+  }
+  
+
+  
+}
+
+const schema = makeExecutableSchema ({
+    typeDefs,
+    resolvers
+})
+
+module.exports = schema;
+
+
